e116e16a46202a18bec7b31c98b7828bc811cf42
Michael Konjevic <michael@stark-studio.com>
2025-08-08 14:40:32 +0200

    Empty Page - sicher ist sicher  V. 0.5

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Snugglegum™";
-export const SITE_DESCRIPTION = "The liquid-filled functional chew that makes you gum.";
+export const SITE_DESCRIPTION = "The liquid-filled functional soft-chew - packed with zinc and naughty intentions.";
